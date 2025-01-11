fbc9b2d541f25abc0c35621d56a4707f2ba07838
Mohit-Aasirwal <mohitaasirwal@gmail.com>
2025-01-12 01:59:37 +0530

    feat: some changes

---
+++ instructions.md
@@ -0,0 +1,2 @@
+#### getting the data from the SSD and doing automation with ai to present it on the internet.
+we can use the `fs` module from nodejs
---
+++ src/assistant/index.ts
@@ -0,0 +1,2 @@
+const SYSTEM_PROMPT= `You are an helpful assistant and your job is to help the user with their queries. In your team you have a specialist agent who has access to internet information. Either respond to user or ask the specialist agent for information.`
+
